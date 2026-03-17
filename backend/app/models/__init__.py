bb937214841ebeccd3a4400679af93ca6449e283
krishkavathia27-1364 <krishkavathia27@gmail.com>
2026-03-17 20:34:35 +0530

    fix: remove obsolete PlaylistItem import from models init

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,7 +1,7 @@
 from .user import User  # noqa: F401
 from .screen import Screen  # noqa: F401
 from .media import Media  # noqa: F401
-from .playlist import Playlist, PlaylistItem  # noqa: F401
+from .playlist import Playlist  # noqa: F401
 from .license import License  # noqa: F401
 from .webhook import Webhook  # noqa: F401
 from .schedule import Schedule  # noqa: F401
