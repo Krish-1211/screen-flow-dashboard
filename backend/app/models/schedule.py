cd34f5cea7db6ea8df07efd34cc5877bf6dfa62a
krishkavathia27-1364 <krishkavathia27@gmail.com>
2026-03-21 19:57:30 +0530

    Fix SQLAlchemy ARRAY definition in Python model

--- backend/app/models/schedule.py
+++ backend/app/models/schedule.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import uuid
 from sqlalchemy import Column, String, DateTime, text, Integer, Boolean
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.dialects.postgresql import UUID, ARRAY
 
 from ..database import Base
 
@@ -15,7 +15,7 @@ class Schedule(Base):
     day = Column(String, nullable=True) # e.g. "Monday" (Keeping for transition)
     start_hour = Column(Integer, nullable=True) # (Keeping for transition)
     end_hour = Column(Integer, nullable=True)   # (Keeping for transition)
-    days_of_week = Column(text('int[]'), nullable=True) # Postgres integer array
+    days_of_week = Column(ARRAY(Integer), nullable=True) # Postgres integer array
     start_time = Column(String, nullable=True, default="09:00:00")
     end_time = Column(String, nullable=True, default="17:00:00")
     active = Column(Boolean, default=True, nullable=False)
